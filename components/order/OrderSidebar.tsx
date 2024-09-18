a4be6f7560fbc5f13042ccf630da6cff3cf15ee3
irenemonzon <irene.monzonm@gmail.com>
2024-09-19 08:52:56 +1000

    get data from prisma, create conexion to get data

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,9 +1,18 @@
-const OrderSidebar = () => {
+import { prisma } from "@/src/lib/prisma"
+
+async function getCategories(){
+  return await prisma.category.findMany()
+}
+
+export default async function OrderSidebar () {
+
+  const categories=await getCategories()
+  console.log(categories)
+ 
+
   return (
     <aside className="md:w-72 md:h-screen bg-white">
         OrderSidebar
     </aside>
   )
 }
-
-export default OrderSidebar
