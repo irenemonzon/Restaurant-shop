3a4ba5ce99f5e49dd6b8fa7a79cb3833eb538965
irenemonzon <irene.monzonm@gmail.com>
2024-09-19 09:35:41 +1000

    get products with relation product and category

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { prisma } from "@/src/lib/prisma"
 
-const OrderPage = () => {
+async function getProducts(category:string){
+  const products=await prisma.product.findMany({
+    where:{
+      category:{
+        slug:category
+      }
+    }
+  })
+  return products
+}
+
+const OrderPage = async({params}:{params:{category:string}}) => {
+
+  const products=await getProducts(params.category)
+  console.log(products)
+
   return (
     <div>OrderPage</div>
   )
