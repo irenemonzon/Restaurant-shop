52e039acbc7d4e5e4f8e9615eaad0e853bbd9d78
irenemonzon <irene.monzonm@gmail.com>
2024-09-19 10:08:29 +1000

    get products and show products by category

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "@/components/products/ProductCard"
 import { prisma } from "@/src/lib/prisma"
 
 async function getProducts(category:string){
@@ -14,10 +15,24 @@ async function getProducts(category:string){
 const OrderPage = async({params}:{params:{category:string}}) => {
 
   const products=await getProducts(params.category)
-  console.log(products)
 
   return (
-    <div>OrderPage</div>
+    <>
+      <h1 className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuación
+      </h1>
+      <div 
+        className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start"
+      >
+        {products.map(product=>(
+          <ProductCard
+          key={product.id}
+          product={product}
+          />
+        ))}
+
+      </div>
+    </>
   )
 }
 
--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,14 +1,20 @@
-import { Category } from "@prisma/client"
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
+import { Category } from "@prisma/client"
 
 type CategoryIconProps={
     category:Category
 }
 
 const CategoryIcon = ({category}:CategoryIconProps) => {
+
+    const params=useParams<{category:string}>()
+
   return (
-    <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+    <div className={`${category.slug === params.category ? 'bg-amber-400':'' } flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
         <div className="w-20 h-20 relative">
 
             <Image 
