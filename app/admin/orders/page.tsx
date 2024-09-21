dab08908846476587d72af56c0181a5d00f7a78a
irenemonzon <irene.monzonm@gmail.com>
2024-09-22 08:52:33 +1000

    create page to admin for the order and change logo

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,15 @@
+import Heading from '@/components/ui/Heading'
+import React from 'react'
+
+const OrderPage = () => {
+  return (
+    <>
+    <Heading>
+        Administrar ordenes
+    </Heading>
+
+    </>
+  )
+}
+
+export default OrderPage
---
+++ components/ui/Logo.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='flex justify-center mt-5'>
+        <div className='relative w-40 h-40'>
+            <Image
+                fill
+                alt="Logo fresh Coffe"
+                src='/logo.svg'
+            />
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Logo
