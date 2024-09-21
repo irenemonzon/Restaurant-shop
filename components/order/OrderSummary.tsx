e0419ff546d9bb0c408f131751cd89df8447c10e
irenemonzon <irene.monzonm@gmail.com>
2024-09-21 10:30:11 +1000

    create form with action to order products and install zod to validate name or any data from the model

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -4,11 +4,26 @@ import { useStore } from "@/src/store"
 import ProductDetail from "./ProductDetail"
 import { useMemo } from "react"
 import { formatCurrency } from "@/src/utils"
+import { createOrder } from "@/actions/create-order-action"
+import { OrderSchema } from "@/src/schema"
 
 const OrderSummary = () => {
 
   const order=useStore((state)=>state.order)
   const total= useMemo(()=> order.reduce((total,item)=>total +(item.quantity * item.price),0),[order])
+
+  const handleCreateOrder=(formData:FormData)=>{
+
+    const data={
+      name:formData.get('name')
+
+    }
+    const result= OrderSchema.safeParse(data)
+    console.log(result)
+    return
+    createOrder()
+
+  }
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
@@ -30,6 +45,24 @@ const OrderSummary = () => {
         {formatCurrency(total)}
           </span>
         </p>
+        <form   
+          className="w-full mt-10 space-y-5"
+          action={handleCreateOrder}
+        >
+          <input
+            type="text"
+            placeholder="Name"
+            className="bg-white border border-gray-100 p-2 w-full"
+            name='name'
+
+          />
+          <input
+            type="submit"
+            className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+            value='Confirmar Pedido'
+          />
+
+        </form>
       </div>
       }
 
